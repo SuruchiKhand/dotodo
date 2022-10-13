890986350f37b18f42c6c8f9a7c9c5720754b7bc
Suruchi Khand <khand.suruchi@gmail.com>
2022-10-13 11:01:21 -0400

    fixed issue in settings.py

--- backend/dotodo_proj/settings.py
+++ backend/dotodo_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e)x-)p2qhp!@*2un=t+w1f@f@cgjwb=+7a@91%fw+l0dee*)y5'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
